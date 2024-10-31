e788d43074a45d2b531c1db8a775c3e05a5332af
s230291 <gimpo.s230291@ggh.goe.go.kr>
2024-10-31 15:29:13 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -13,7 +13,7 @@ col1, col2 = st.columns(2)
 # 왼쪽 문단
 with col1:
       st.subheader('**개념정리')
-     st.write('- 양수: 0보다 큰 수')
+      st.write('- 양수: 0보다 큰 수')
       st.write('- 음수: 0보다 작은 수')
       st.write('- 양수: (+) / 음수: (-) 부호가 붙음')
       st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
